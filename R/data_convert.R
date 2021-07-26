c383e775b23ac61dab76a1f60766dd02c5b1848a
Alex Towell <queelius@gmail.com>
2021-07-25 21:42:20 -0500

    updates

--- R/data_convert.R
+++ R/data_convert.R
@@ -1,18 +1,25 @@
 #' Convert counts to individual responses
 #'
 #' There are many ways to represent the same data. In problem 4.2, the data
-#' is given in the form of counts, e.g., counts[j] represents the number
-#' of people who responded with j risky sexual encounters.
-#' The more natural form of this data is as a vector of indivual responses,
-#' e.g., x[k] is the k-th persons response to the number of risky sexual
-#' encounters.
-#' @param counts Count data to convert
+#' is given in the form of counts. This is the inverse of
+#' em.counts_to_responses, e.g.,
+#' ```
+#'  xs <- em.counts_to_responses(counts)
+#'  em.responses_to_counts(xs) == counts
+#'  em.counts_to_responses(em.responses_to_counts(xs)) == xs.
+#'```
+#' @param counts count data
+#' @return the count data converted to response data, say counts = (379,299,...),
+#'         then 379 responded 0 encounters, 299 responded 1 encounter, ...
 #' @keywords counts multinomial responses
+#' @examples
+#'   # let counts be the count data
+#'   counts[j] # denotes number of respondents with j risky sexual encounters.
+#'   xs <- em.counts_to_responses(counts)
+#'   xs[k] # denotes the response of the i-th person
 #' @export
 em.counts_to_responses <- function(counts)
 {
-  # say counts = (379,299,...), then 379 responded 0 encounters, 299 responded
-  # 1 encounter, and so on.
   data <- NULL
   for (i in 1:length(counts))
   {
@@ -21,15 +28,16 @@ em.counts_to_responses <- function(counts)
   data
 }
 
-#' Convert individual responses to count data
+#' Convert individual response data to count data
 #'
 #' This is the inverse of em.counts_to_responses, e.g.,
 #'   em.responses_to_counts(em.counts_to_responses(counts)) == counts
 #' and
 #'   em.counts_to_responses(em.responses_to_counts(data)) == data.
-#' @param data Vector of responses
-#' keywords counts multinomial responses
-#' export
+#' @param data response data
+#' @returns response data converted to count data
+#' @keywords counts multinomial responses
+#' @export
 em.responses_to_counts <- function(data)
 {
   counts <- NULL
